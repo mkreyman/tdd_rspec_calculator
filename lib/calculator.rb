079b4cf3adc3c337ffa62a1f5f2d0f93ec3da1f4
Mark Kreyman <mark@kreyman.com>
2014-02-22 10:05:43 -0700

    Added .subtract to my fork of tdd_rspec_calculator

---
+++ lib/calculator.rb
@@ -0,0 +1,20 @@
+class Calculator
+  def initialize
+    @total = 0
+  end
+
+  def total
+    @total
+  end
+
+  def add(number)
+    @total += number
+    self
+  end
+
+  def subtract(number)
+    @total -= number
+    self
+  end
+
+end
--- spec/lib/calculator_spec.rb
+++ spec/lib/calculator_spec.rb
@@ -1,4 +1,75 @@
+require 'rspec'
 require './lib/calculator'
 
 describe Calculator do
+  describe ".total" do
+    it "should be 0 on a new calculator" do
+      expect(subject.total).to eq(0)
+    end
+  end
+
+  describe ".add" do
+    it "should add a number to the total" do
+      subject.add(5)
+      expect(subject.total).to eq(5)
+    end
+
+    it "should handle multiple calls to add" do
+      subject.add(5).add(2)
+      expect(subject.total).to eq(7)
+    end
+
+    it "should handle negative numbers" do
+      subject.add(-2)
+      expect(subject.total).to eq(-2)
+
+      subject.add(5).add(-27).add(10).add(-12)
+      expect(subject.total).to eq(-26)
+    end
+
+    it "should handle floats" do
+      subject.add(2.5)
+      expect(subject.total).to eq(2.5)
+
+      subject.add(-3.1).add(4.5).add(-31.257)
+      expect(subject.total).to be_within(0.0001).of(-27.357)
+    end
+
+    # add(5).add(2)
+    # add(5,2)
+  end
+
+  describe ".subtract" do
+    it "should subtract a number from the total" do
+      subject.add(10)
+      subject.subtract(5)
+      expect(subject.total).to eq(5)
+    end
+
+    it "should handle multiple calls to subtract" do
+      subject.add(10)
+      subject.subtract(5).subtract(2)
+      expect(subject.total).to eq(3)
+    end
+
+    it "should handle negative numbers" do
+      subject.subtract(-2)
+      expect(subject.total).to eq(2)
+
+      subject.subtract(5).subtract(-27).subtract(10).subtract(-12)
+      expect(subject.total).to eq(26)
+    end
+
+    it "should handle floats" do
+      subject.add(5)
+      subject.subtract(2.5)
+      expect(subject.total).to eq(2.5)
+
+      subject.subtract(-3.1).subtract(9.5).subtract(-31.257)
+      expect(subject.total).to be_within(0.0001).of(27.357)
+    end
+
+    # add(5).add(2)
+    # add(5,2)
+  end
 end
